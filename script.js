d4320a3c237579dc995e7592fbc9c5d5b44ad520
zekemntmr <zekemntmr@gmail.com>
2025-11-13 16:01:18 +0800

    added another div for much better viewing

--- script.js
+++ script.js
@@ -1,59 +1,65 @@
-    function initMap(mapDataUrl){
-        const map = L.map('map').setView([8.390397295307483, 124.88526599138835],17);
+function initMap(mapDataUrl) {
+  const map = L.map('map').setView([8.390397295307483, 124.88526599138835], 17);
 
-    const openStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
-        maxZoom:12,
-        attribution:'&copy; OpenStreetMap contributors'
-    });
+  const openStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 12,
+    attribution: '&copy; OpenStreetMap contributors'
+  });
 
-    const satellite = L.tileLayer(
-        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-        {attribution:'Tiles (c) Esri - Source: Esri, Maxar, Earthstar Geographics, GIS User Community'}
-    );   
+  const satellite = L.tileLayer(
+    'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    { attribution: 'Tiles © Esri - Source: Esri, Maxar, Earthstar Geographics' }
+  );
 
+  openStreetMap.addTo(map);
+  console.log("Base map tile layer loaded " + Date());
 
-    openStreetMap.addTo(map);
-    console.log("Base map tile layer loaded" + Date());
+  const markerGroups = {};
 
-    const markerGroups ={};
+  fetch(mapDataUrl)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(pin => {
+        const type = pin.locationType || "Other";
 
-    fetch(mapDataUrl)
-        .then(response => response.json())
-        .then(data => {
-            data.forEach( pin => {
-                const type = pin.locationType || "Other";
+        if (!markerGroups[type]) {
+          markerGroups[type] = L.layerGroup();
+        }
 
-                if(!markerGroups[type]){
-                    markerGroups[type] = L.layerGroup();
-                }
+        const marker = L.marker([pin.lat, pin.lng]);
 
-                const marker = L.marker([pin.lat,pin.lng]).addTo(map);
-                const img = `<img src='${pin.img}' class="popup-image">`;
-                marker.bindPopup(`${img}<br><b> ${pin.title} </b><br> ${pin.description}<br> ${pin.locationType}`);
-                console.log(`${pin.name} Loaded ` +Date());
+        // When marker is clicked, update the info panel
+        marker.on('click', () => {
+          const infoPanel = document.getElementById('info-panel');
+          infoPanel.innerHTML = `
+            <img src="${pin.img}" alt="${pin.title}">
+            <h3>${pin.title}</h3>
+            <p><strong>Type:</strong> ${pin.locationType}</p>
+            <p>${pin.description}</p>
 
-                markerGroups[type].addLayer(marker);
-            });
+          `;
+        });
 
+        markerGroups[type].addLayer(marker);
+      });
 
-            Object.values(markerGroups).forEach(group => group.addTo(map));
+      Object.values(markerGroups).forEach(group => group.addTo(map));
 
-            const baseMaps={
-                "OpenStreetMap":openStreetMap,
-                "Satellite": satellite
-            }
+      const baseMaps = {
+        "OpenStreetMap": openStreetMap,
+        "Satellite": satellite
+      };
 
-            const overlayMaps={};
+      const overlayMaps = {};
+      Object.keys(markerGroups).forEach(type => {
+        overlayMaps[type] = markerGroups[type];
+      });
 
-            Object.keys(markerGroups).forEach(type=>{
-                overlayMaps[type] = markerGroups[type];
-            });
-            
+      L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(map);
+    })
+    .catch(err => console.error('Error loading JSON', err));
+}
 
-            L.control.layers(baseMaps,overlayMaps,{collapsed:false}).addTo(map);
-        })
-        .catch(err => console.error('Error loading JSON', err));
-    }
-document.addEventListener("DOMContentLoaded",()=>{
-    initMap('maps.json');
-;});
+document.addEventListener("DOMContentLoaded", () => {
+  initMap('maps.json');
+});
